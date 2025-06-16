10da659a1df252c9b3aec70e7c2c850e341a81f0
Salvadorvga2004 <salvadorvgac04@gmail.com>
2025-06-15 21:48:50 -0600

    no

--- practica4s/src/App.js
+++ practica4s/src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Layout from './Layout'; 
-import './App.css'; 
- function Home() {  return <h1>Bienvenidos a la Página de Inicio</h1>; 
-}  function About() {  return <h2>Acerca de Mi</h2>; 
-} 
- 
-function App() {  return ( 
-   <BrowserRouter> 
-     <Routes> 
-       <Route path="/" element={<Layout />}> 
-         <Route index element={<Home />} /> 
-         <Route path="about" element={<About />} /> 
-       </Route> 
-     </Routes> 
-   </BrowserRouter> 
-   
- ); 
-}  
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import './App.css';
+
+function Home() {
+  return <h1>Bienvenidos a la Página de Inicio</h1>;
+}
+function About() {
+  return <h2>Acerca de Mi</h2>;
+}
+
+function App() {
+  return (
+    <BrowserRouter basename="/practica4s">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
